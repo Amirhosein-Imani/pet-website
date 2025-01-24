be7a3f7e9ce00c864d6dcb8f075ae7f4da7a1a0f
Amirhosein Imani <imaniamirhosein2001@gmail.com>
2025-01-24 15:10:57 +0330

    Finish header

--- main.js
+++ main.js
@@ -19,3 +19,39 @@ navLinks.addEventListener("click" , (e) => {
     menuBtnIcon.setAttribute("class" , "ri-menu-line")
 
 })
+
+const scrollRevealOption = {
+
+    distance : "50px" , origin : "bottom" , duration : 1000 
+
+}
+
+ScrollReveal().reveal(".header__content h4" , {
+
+    ...scrollRevealOption 
+
+})
+
+ScrollReveal().reveal(".header__content h1" , {
+
+    ...scrollRevealOption , delay : 500
+
+})
+
+ScrollReveal().reveal(".header__content h2" , {
+
+    ...scrollRevealOption , delay : 1000
+
+})
+
+ScrollReveal().reveal(".header__content p" , {
+
+    ...scrollRevealOption , delay : 1500
+
+})
+
+ScrollReveal().reveal(".header__btn" , {
+
+    ...scrollRevealOption , delay : 2000
+
+})
